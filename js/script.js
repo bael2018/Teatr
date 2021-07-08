a2553c5cdd5649477742aff7c42d4d1c792fc95a
bael <osh.kg1819@gmail.com>
2021-07-08 13:37:27 +0600

    370px

--- js/script.js
+++ js/script.js
@@ -13,3 +13,26 @@ langTwo.addEventListener('click' , e => {
     langTwo.classList.add('activeLanguage')
     langOne.classList.remove('activeLang')
 })
+
+// burgerButton
+
+const burgerContainer = document.querySelector('.burger_menu')
+const burgerButton = document.querySelector('.burgerButton')
+const burgerButtonAlt = document.querySelector('.burgerButtonAnother')
+burgerButton.addEventListener('click' , e => {
+    e.preventDefault() 
+    burgerContainer.classList.add('activeBurger')
+})
+burgerButtonAlt.addEventListener('click' , e => {
+    e.preventDefault() 
+    burgerContainer.classList.add('activeBurger')
+})
+
+// closeBurgerBtn
+
+const closeBurger = document.querySelector('.burgerCloseBtn')
+
+closeBurger.addEventListener('click' , e => {
+    e.preventDefault()
+    burgerContainer.classList.remove('activeBurger')
+})
